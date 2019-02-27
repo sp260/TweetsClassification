809dad43ebda24c48ce4b492f9eeb740b3bf165e
sp260 <sofienesp260@gmail.com>
2019-02-27 11:46:14 +0100

    first extraction

---
+++ app.py
@@ -0,0 +1,22 @@
+from TwitterAPI import TwitterAPI
+api = TwitterAPI('APIKey', 
+                'APISecretKey', 
+                'AccessToken', 
+                'AccessTokenSecret')
+
+r = api.request('search/tweets', {'q':'Macron', 'count':200, 'tweet_mode': 'extended'})
+
+corpus = ''
+
+for item in r.get_iterator():
+    tweetId = item['id']
+    if 'retweeted_status' in item:
+        tweetText = item['retweeted_status']['full_text']
+        corpus += '(' + str(tweetId) + ', ???)' + tweetText.replace('\n', ' ') + '\n'
+    else:
+        tweetText = item['full_text']
+        corpus += '(' + str(tweetId) + ', ???)' + tweetText.replace('\n', ' ') + '\n'
+
+file = open('TweetsAboutMacron.txt','w')
+file.write(corpus) 
+file.close() 
