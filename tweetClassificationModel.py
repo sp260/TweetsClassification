3807494e2d81e674e62dd4a8da87f5f93c32df46
sp260 <sofienesp260@gmail.com>
2019-05-01 00:40:00 +0200

    error not fixed yet for model test

--- tweetClassificationModel.py
+++ tweetClassificationModel.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[54]:
-
-
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
@@ -19,9 +13,10 @@ import pickle
 import codecs
 import spacy
 import glob
+import sys
 import re
 
-all_files = glob.glob("corpus.txt")
+all_files = glob.glob("../final_promo/groupe*.txt")
 df = pd.DataFrame()
 for file_ in all_files: 
     list_of_lists = []
@@ -39,17 +34,6 @@ for file_ in all_files:
     df = df.append(next_df)
 df = df.drop_duplicates()
 df = df.reset_index(drop=True)
-df.info()
-
-
-# In[55]:
-
-
-print(df.Sentiment.value_counts())
-
-
-# In[56]:
-
 
 contraction_mapping = {
     "gôche": "gauche",
@@ -66,10 +50,10 @@ contraction_mapping = {
     " 1 ": " un ",
     "PR": "président",
     "RT": "retweet",
-    "LREM": "la republique en marche",
-    "LAREM": "la republique en marche",
-    "#LREM": "la republique en marche",
-    "#LAREM": "la republique en marche",
+    "LREM": "la république en marche",
+    "LAREM": "la république en marche",
+    "#LREM": "la république en marche",
+    "#LAREM": "la république en marche",
     "telma": "tellement",
     "ctr": "contre",
     "5e": "cinquieme",
@@ -79,12 +63,7 @@ contraction_mapping = {
     "b8en": "bien",
     "good": "bien",
     "bad": "mauvais",
-    "lui": "il"
 }
-
-
-# In[57]:
-
 
 nlp = spacy.load('fr')
 
@@ -108,200 +87,114 @@ def tweet_cleaner(text):
     spell_corrected = re.sub(r'(.)\1+', r'\1\1', joined)
     return spell_corrected
 
-
-# In[58]:
-
-
-pd.set_option('display.max_colwidth', -1)
-print(df.Tweet[131:135])
-
-
-# In[59]:
-
-
-print([tweet_cleaner(t) for t in df.Tweet[131:135]])
-
-
-# In[60]:
-
-
+#Clean the data
 df['Clean_tweet'] = [tweet_cleaner(t) for t in df.Tweet]
-
-
-# In[61]:
-
-
-df.head()
-
-
-# In[62]:
-
 
 #Get stopwords from file
 with open('./stop_words_fr.txt') as f:
     stopwords = f.read().splitlines()
 
-
-# In[63]:
-
-
+#Transform the data
 X = df.Clean_tweet.values
 y = df.Sentiment.values
 
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(X)
 
-# split train & test
-training_X, test_X, training_y, test_y = train_test_split(X, y, test_size = .3, random_state=42)
+def example_cleaning():
 
-sm = SMOTE(random_state=42)
-X_train_s, y_train_s = sm.fit_sample(training_X, training_y)
+    pd.set_option('display.max_colwidth', -1)
+    print(df.Tweet[131:135])
 
-X_train, X_test, y_train, y_test = train_test_split(X_train_s, y_train_s, test_size = .3, random_state=42)
+    print([tweet_cleaner(t) for t in df.Tweet[131:135]])
 
-# model tuning & validation
-cv = StratifiedKFold()
+def train():
+    df.info()
+    print(df.Sentiment.value_counts())
 
+    # split train & test
+    training_X, test_X, training_y, test_y = train_test_split(X, y, test_size = .3, random_state=42)
 
-# In[64]:
+    sm = SMOTE(random_state=42)
+    X_train_s, y_train_s = sm.fit_sample(training_X, training_y)
 
+    X_train, X_test, y_train, y_test = train_test_split(X_train_s, y_train_s, test_size = .3, random_state=42)
 
-rf_grid = {  
-     #'max_depth': list(range(10,100,10)),
-     #'max_features': ['auto', 'sqrt'],
-     #'min_samples_leaf': [1, 2, 4],
-     #'min_samples_split': [2, 5, 10],
-     'n_estimators': list(range(100,500,100))
-}
-                  
-rf_clf = RandomForestClassifier(n_estimators=35, max_depth=8, min_samples_split=4,n_jobs=-1)
+    # model tuning & validation
+    cv = StratifiedKFold()
 
-best_rf_clf = GridSearchCV(rf_clf, rf_grid, verbose=2,cv=cv)
-best_rf_clf.fit(X_train, y_train)
-rf_preds = best_rf_clf.predict(test_X)
+    rf_grid = {  
+        'max_depth': list(range(10,100,10)),
+        'min_samples_leaf': [1, 2, 4],
+        'min_samples_split': [2, 5, 10],
+        'n_estimators': list(range(100,500,100))
+    }
+                    
+    rf_clf = RandomForestClassifier(n_estimators=35, max_depth=8, min_samples_split=4,n_jobs=-1)
 
-print("Random Forest")
-print(best_rf_clf.best_params_)
-print(classification_report(test_y, rf_preds))
-print("Accuracy", accuracy_score(test_y, rf_preds))
+    best_rf_clf = GridSearchCV(rf_clf, rf_grid, verbose=2,cv=cv)
+    best_rf_clf.fit(X_train, y_train)
+    rf_preds = best_rf_clf.predict(test_X)
 
+    print("Random Forest")
+    print(best_rf_clf.best_params_)
+    print(classification_report(test_y, rf_preds))
+    print("Accuracy", accuracy_score(test_y, rf_preds))
 
-# In[65]:
+    lr_grid = {
+        "C": np.logspace(-4,4,20), 
+        "penalty": ["l1","l2"] # l1 lasso l2 ridge
+    }
 
+    lr_clf = LogisticRegression()
 
-lr_grid = {
-    "C": np.logspace(-4,4,20), 
-    "penalty": ["l1","l2"] # l1 lasso l2 ridge
-}
+    best_lr_clf = GridSearchCV(lr_clf, lr_grid, verbose=2,cv=cv)
+    best_lr_clf.fit(X_train, y_train)
+    lr_preds = best_lr_clf.predict(test_X)
 
-lr_clf = LogisticRegression()
+    print("Logistic Regression")
+    print(best_lr_clf.best_params_)
+    print(classification_report(test_y, lr_preds))
+    print("Accuracy", accuracy_score(test_y, lr_preds))
 
-best_lr_clf = GridSearchCV(lr_clf, lr_grid, verbose=2,cv=cv)
-best_lr_clf.fit(X_train, y_train)
-lr_preds = best_lr_clf.predict(test_X)
+    # Save rf classifier to a file
+    save_classifier = open("rf_classifier.pickle", 'wb')
+    pickle.dump(best_rf_clf, save_classifier)
+    save_classifier.close()
 
-print("Logistic Regression")
-print(best_lr_clf.best_params_)
-print(classification_report(test_y, lr_preds))
-print("Accuracy", accuracy_score(test_y, lr_preds))
+    # Save lr classifier to a file
+    save_classifier = open("lr_classifier.pickle", 'wb')
+    pickle.dump(best_lr_clf, save_classifier)
+    save_classifier.close()
 
+def example_oversampling():
+    #Example of oversamling with SMOTE
 
-# In[66]:
+    sent1 = "emmanuel macron etre bon"
+    sent2 = "macron etre un monstre"
+    sent3 = "macron sale merde"
+    sent4 = "comme il etre mauvais ce macron"
+    sent5 = "il avoir bien aimer macron"
 
+    testing_text = pd.Series([sent1, sent2, sent3, sent4, sent5])
+    testing_target = pd.Series([1,0,0,0,1])
 
-# Save rf classifier to a file
-save_classifier = open("rf_classifier.pickle", 'wb')
-pickle.dump(best_rf_clf, save_classifier)
-save_classifier.close()
+    tv = TfidfVectorizer(stop_words=None, max_features=100000)
+    testing_tfidf = tv.fit_transform(testing_text)
 
-# Save lr classifier to a file
-save_classifier = open("lr_classifier.pickle", 'wb')
-pickle.dump(best_lr_clf, save_classifier)
-save_classifier.close()
+    smt = SMOTE(random_state=777, k_neighbors=1)
+    X_SMOTE, y_SMOTE = smt.fit_sample(testing_tfidf, testing_target)
+    df_topredict = pd.DataFrame(X_SMOTE.todense(), columns=tv.get_feature_names())
+    df_topredict
 
+    container_df = pd.DataFrame(0.0, index=np.arange(len(X_SMOTE.todense())), columns=vectorizer.get_feature_names())
 
-# In[67]:
-
-
-# Retrieve the saved file and upload it to an object
-vec = open("rf_classifier.pickle", 'rb')
-rf_clf = pickle.load(vec)
-vec.close()
-
-# Retrieve the saved file and upload it to an object
-vec = open("lr_classifier.pickle", 'rb')
-lr_clf = pickle.load(vec)
-vec.close()
-
-
-# In[68]:
-
-
-#Example of oversamling with SMOTE
-
-sent1 = "emmanuel macron etre bon"
-sent2 = "macron etre un monstre"
-sent3 = "macron sale merde"
-sent4 = "comme il etre mauvais ce macron"
-sent5 = "il avoir bien aimer macron"
-
-testing_text = pd.Series([sent1, sent2, sent3, sent4, sent5])
-testing_target = pd.Series([1,0,0,0,1])
-
-tv = TfidfVectorizer(stop_words=None, max_features=100000)
-testing_tfidf = tv.fit_transform(testing_text)
-
-smt = SMOTE(random_state=777, k_neighbors=1)
-X_SMOTE, y_SMOTE = smt.fit_sample(testing_tfidf, testing_target)
-df_topredict = pd.DataFrame(X_SMOTE.todense(), columns=tv.get_feature_names())
-df_topredict
-
-
-# In[69]:
-
-
-print(y_SMOTE)
-
-
-# In[70]:
-
-
-pd.DataFrame(test_X.todense(), columns=vectorizer.get_feature_names()).info()
-
-
-# In[71]:
-
-
-container_df = pd.DataFrame(0.0, index=np.arange(len(X_SMOTE.todense())), columns=vectorizer.get_feature_names())
-
-
-# In[72]:
-
-
-for column in df_topredict:
-    if column in vectorizer.get_feature_names():
-        container_df[column] = df_topredict[column]
-
-
-# In[73]:
-
-
-preds = best_lr_clf.predict(container_df)
-preds
-
-
-# In[74]:
-
-
-preds = best_rf_clf.predict(container_df)
-preds
-
-
-# In[77]:
-
+    for column in df_topredict:
+        if column in vectorizer.get_feature_names():
+            container_df[column] = df_topredict[column]
 
 def get_predictions(file):    
-    # Convert the file corpus to a dataframe
+    #Convert the file corpus to a dataframe
     list_of_lists = []
     with open(file, 'rU') as f:
         for line in f:
@@ -315,32 +208,38 @@ def get_predictions(file):
             list_of_lists.append([id, sentiment, group, tweet])
     df = pd.DataFrame(list_of_lists, columns=['Id','Sentiment', ' ', 'Tweet'])
     print(df.info())
-    print(df.head())
     
-    # Clean and transform the data
+    #Clean and transform the data
     df['Clean_tweet'] = [tweet_cleaner(t) for t in df.Tweet]
     
     X = df.Clean_tweet.values
     y = df.Sentiment.values
     
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(X)
+    tv = TfidfVectorizer()
+    X = tv.fit_transform(X)
     
-    # Use the saved classifier and evaluate the predictions
+    #Use the saved classifier and evaluate the predictions
     vec = open("rf_classifier.pickle", 'rb')
     clf = pickle.load(vec)
     vec.close()
+    
+    #Use a container for the dataframe to make the prediction
+    container_df = pd.DataFrame(0.0, index=np.arange(len(X.todense())), columns=vectorizer.get_feature_names())
+    df_topredict = pd.DataFrame(X.todense(), columns=tv.get_feature_names())
+    for column in df_topredict:
+        if column in vectorizer.get_feature_names():
+            container_df[column] = df_topredict[column]
 
-    # predict on the new data
-    preds = clf.predict(X)
+    preds = clf.predict(container_df)
+    print(preds)
 
-    # If the file is already annoted, we can evaluate the prediction
-    if df.Sentiment.values[0]!='???':
-        print(classification_report(y, preds))
-        print("Accuracy", accuracy_score(y, preds))
+    #If the file is already annoted, we can evaluate the prediction by decommenting the two following lines
+    #print(classification_report(y, preds))
+    #print("Accuracy", accuracy_score(y, preds))
+
+for file in sys.argv[1:]:
+    print('*** Analyse du sentiment pour le fichier', file[:-4], '***')
+    get_predictions(file)
+    print('----------  ----------  ----------')
 
 
-# In[78]:
-
-FILE = 'TweetsAboutMacron.txt'
-get_predictions(FILE)
